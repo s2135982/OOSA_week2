7e459fd773c2c92be73bd77f94a1b640b72e1ccf
s2135982 <s2135982@ed.ac.uk>
2022-01-26 00:20:02 +0000

    fix induntion and plot

--- newSearchObject.py
+++ newSearchObject.py
@@ -13,68 +13,82 @@ from sys import exit
 #####################################
 
 class dataSorter(object):
-  '''A class to hold data and provide examples of search algorithms'''
+    '''A class to hold data and provide examples of search algorithms'''
 
-  def __init__(self,numb=100):
-    '''Class initialiser'''
-    self.numb=numb
-    '''为什么这里是（numb）?'''
-    self.arr=np.random.random((numb))
+    def __init__(self,numb=100):
+        '''Class initialiser'''
+        self.numb=numb
+        self.arr=np.arange(0, numb, dtype = np.float64)
 
-  def sortArray(self):
-    '''Sort an array'''
-    # preserve original array by copying
-    copArr=np.copy(self.arr)
-    # create workspace
-    self.sortArr=np.empty(self.arr.shape)
-    # sort the array
-    for i in range(0,copArr.shape[0]):
-      minN,minInd=self.findMin(copArr)
-      self.sortArr[i]=minN
-      copArr[minInd]=10000000
+    def sortArray(self):
+        '''Sort an array'''
+        # preserve original array by copying
+        copArr=np.copy(self.arr)
+        # create workspace
+        self.sortArr=np.empty(self.arr.shape)
+        # sort the array
+        for i in range(0,copArr.shape[0]):
+            minN,minInd=self.findMin(copArr)
+            self.sortArr[i]=minN
+            copArr[minInd]=10000000
 
-  def findMin(self,arr):
-    '''A minimum finding function'''
-    minN=10000000  # a big number
-    minInd=0
-    for i in range(0,arr.shape[0]):
-      if(arr[i]<minN):
-        minN=arr[i]
-        minInd=i
-    return(minN,minInd)
+    def findMin(self,arr):
+        '''A minimum finding function'''
+        minN=10000000  # a big number
+        minInd=0
+        for i in range(0,arr.shape[0]):
+            if(arr[i]<minN):
+                minN=arr[i]
+                minInd=i
+        return(minN,minInd)
 
-  def plotter(self):
-      '''Add a function to plot a linegraph of the sorted array (y axis) against array index a axis'''
-    plt.plot(self.arr)
-    plt.show()
-
+    def plotter(self):
+        '''Add a function to plot a linegraph of the sorted array (y axis) against array index a axis'''
+        plt.plot(self.arr)
+        plt.plot(self.sortArr)
+        plt.legend('arr', 'sortArr')
+        plt.show()
     
-def binarySearch(self,target):
-    '''Binary search for target in x by looping'''
-    x = self.arr
-    start = 0
-    end = len(self.arr)
+    def binarySearch(self.sortArray, target):
 
-    mid = (start + end)//2
+        '''Binary search for target in x by looping'''
+        x = self.arr
+        start = 0
+        end = len(self.arr)
 
-    # a flag to tell whether find the target or not 
-    findTarget = False
+        mid = (start + end)//2
+
+        # a flag to tell whether find the target or not 
+        findTarget = False
    
-    while((end-start)>1):
+        while((end-start)>1):
     
-        if(x[mid]<target):   # answer is to the right
-            start=midP
-            midP=(start+end)//2
-        elif(x[mid]>target):   # answer is to the left
-            end=mid
-            mid=(start+end)//2 
-        elif(x[mid]==target):  # found answer, unlikely
-            foundExact=True  # have found exact answer
-            break
+            if(x[mid]<target):   # answer is to the right
+                start=mid
+                mid=(start+end)//2
+            elif(x[mid]>target):   # answer is to the left
+                end=mid
+                mid=(start+end)//2 
+            elif(x[mid]==target):  # found answer, unlikely
+                findTarget=True  # have found exact answer
+                break
     
-    # if we have not found the exact answer, find the closest element
-    if(findTarget==False):
-        sep=np.abs(x[start:end]-v)  # use a slice to find the absolute distance
-        mid=sep.argmin()+start     # set mid point as element with smallest distance
+        # if we have not found the exact answer, find the closest element
+        if(findTarget==False):
+            sep=np.abs(x[start:end+1]-target)  # use a slice to find the absolute distance
+            mid=sep.argmin()+start     # set mid point as element with smallest distance
 
-    return(x[mid],mid)
+        return(x[mid],mid)
+
+if __name__=="__main__":
+    '''Main block'''
+    s = dataSorter(5)
+    print(s.arr)
+
+    s.sortArray()
+    print(s.sortArr)
+
+    print(s.binarySearch(-2.5))
+
+    s.plotter()
+
